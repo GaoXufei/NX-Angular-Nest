43b09f1bf527c286da900dadda0b6ceac08fee8d
gaoxufei <18047170938@163.com>
2020-05-12 17:24:49 +0800

    refactor

--- apps/gao/src/app/modules/topics/topics.component.ts
+++ apps/gao/src/app/modules/topics/topics.component.ts
@@ -17,7 +17,9 @@ export class TopicsComponent implements OnInit {
     { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
     { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
     { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' }
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+    { text: 'Five', cols: 2, rows: 1, color: '#ccc' },
+    { text: 'six', cols: 2, rows: 1, color: 'red' }
   ];
   constructor() {}
 
