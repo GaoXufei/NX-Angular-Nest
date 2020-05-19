f2e485c22be85e1283f2668330de13a1afb53656
gaoxufei <18047170938@163.com>
2020-05-19 23:48:41 +0800

    feat: [client]网络请求服务

--- apps/gao/src/app/modules/admin/login/login.component.ts
+++ apps/gao/src/app/modules/admin/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+import { RequestService } from '../../request/request.service';
+import { Login } from '@gao/src/app/types/login';
 
 @Component({
   selector: 'nxgao-login',
@@ -13,12 +15,20 @@ export class LoginComponent implements OnInit {
     password: ['', [Validators.required]]
   });
 
-  constructor(private readonly formBuilder: FormBuilder) {}
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly requestService: RequestService
+  ) {}
 
   ngOnInit(): void {}
 
   public async handleLogin() {
     const isValidate = this.registerForm.status;
-    if (isValidate === 'VALID') alert(`提交！`);
+
+    if (isValidate === 'VALID') {
+      const data: Login = this.registerForm.value;
+      const login$ = this.requestService.login(data);
+      login$.subscribe(console.log);
+    }
   }
 }
---
+++ apps/gao/src/app/modules/request/request.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Login, LoginResponse } from '../../types/login';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestService {
+  constructor(private readonly http: HttpClient) {}
+
+  login = (data: Login) => {
+    return this.http.post('/api/auth/login', data).pipe(
+      map((response: LoginResponse) => {
+        const { token } = response;
+        console.log(token);
+        return response;
+      })
+    );
+  };
+}
