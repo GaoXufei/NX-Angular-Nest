5151c0295274dc3af176eacfecdf2d740966539a
gaoxufei <18047170938@163.com>
2020-06-03 17:37:21 +0800

    feat: [client] admin sidebar

---
+++ apps/gao/src/app/components/admin/app-side/app-side.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nxgao-app-side',
+  templateUrl: './app-side.component.html',
+  styleUrls: ['./app-side.component.scss']
+})
+export class AppSideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/gao/src/app/modules/admin/admin.module.ts
+++ apps/gao/src/app/modules/admin/admin.module.ts
@@ -7,12 +7,25 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin.component';
 import { LoginComponent } from './login/login.component';
 
+//#region material modules
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { AppSideComponent } from '../../components/admin/app-side/app-side.component';
+import { UserPanelComponent } from '../../components/admin/app-side/user-panel/user-panel.component';
+const MaterialModules = [MatSidenavModule];
+//#endregion
+
 //#region components
-const COMPONENTS = [DashboardComponent, AdminComponent, LoginComponent];
+const COMPONENTS = [
+  DashboardComponent,
+  AdminComponent,
+  LoginComponent,
+  AppSideComponent,
+  UserPanelComponent
+];
 //#endregion
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, SharedModule, AdminRoutingModule]
+  imports: [CommonModule, SharedModule, ...MaterialModules, AdminRoutingModule]
 })
 export class AdminModule {}
